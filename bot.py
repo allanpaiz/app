991367dc7a2d22998f46bca9a9d0a2b9cb8336e3
allan1371 <allan.paiz0@gmail.com>
2023-01-26 21:04:22 -0700

    learning

--- bot.py
+++ bot.py
@@ -38,5 +38,4 @@ def run_discord_bot():
             await send_message(message, user_message, is_private=False)
 
 
-
     client.run(TOKEN)
--- responses.py
+++ responses.py
@@ -16,18 +16,3 @@ def get_response(message: str) -> str:
             return 'Dolla'
         
         return 'yoooo'
-
-
-def handle_response(message) -> str:
-    p_message = message.lower()
-
-    if p_message == 'hello':
-        return 'Hey there!'
-    
-    if p_message == 'roll':
-        return str(random.randint(1,6))
-    
-    if p_message == '!help':
-        return "`This is a help message.`"
-    
-    return "Unknown Command"
